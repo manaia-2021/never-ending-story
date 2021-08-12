d061935442d00a53a738e9939e82144d724d1e78
kerry-davis <pulsta@gmail.com>
2021-08-13 11:07:49 +1200

    router.get working prototype...

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('express-handlebars');
 const routes = require('./routes');
+const { getStoryData, updateStoryData} = require('./utils')
 
 const server = express();
 
@@ -14,18 +15,5 @@ server.set('view engine', 'hbs');
 
 // Your routes/router(s) should go here
 server.use('/', routes);
-
-server.get ('/', (req, res) =>{
-  const template = 'home'
-  getData((err, initialData) =>{
-    res.render(template, initialData)
-  })
-})
-
-// server.get ('/', (req, res) =>{
-//   res.render ('home')
-// })
-
-
 
 module.exports = server;
