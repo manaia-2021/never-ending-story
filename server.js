2ba110d2d9f4c088231d90b36e55aae59de2b4b5
rodrigo-marcolino <rodrigomarcolino@gmail.com>
2021-08-12 18:17:29 +1200

    clear

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const express = require('express')
-const hbs = require('express-handlebars')
+const express = require('express');
+const hbs = require('express-handlebars');
+const routes = require('./routes');
 
-const server = express()
+const server = express();
 
 // Server configuration
-server.use(express.static('public'))
-server.use(express.urlencoded({ extended: false }))
+server.use(express.static('public'));
+server.use(express.urlencoded({ extended: false }));
 
 // Handlebars configuration
-server.engine('hbs', hbs({ extname: 'hbs' }))
-server.set('view engine', 'hbs')
+server.engine('hbs', hbs({ extname: 'hbs' }));
+server.set('view engine', 'hbs');
 
 // Your routes/router(s) should go here
-
-module.exports = server
+server.use('/', routes);
+module.exports = server;
