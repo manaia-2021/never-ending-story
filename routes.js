28bef4e9dca50e4861706298176f714f3a1e6601
Lee Wannacott <wannacottL@gmail.com>
2021-08-13 11:30:08 +1200

    add check word if statement into post request.

--- routes.js
+++ routes.js
@@ -1,39 +1,43 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
-const { getStoryData } = require('./utils')
+const checkWord = require('check-word');
+const { getStoryData } = require('./utils');
+
+const words = checkWord('en');
 
 const router = express.Router();
 module.exports = router;
 
 router.post('/', (req, res) => {
-  //Create an object of the updated story data from the request body
-  const newWord = req.body
-  // Read in the JSON file and locate the puppy we are going to update
-  getStoryData((err, storyData) => {
-    if (err) {
-      res.status(500).send(err.message)
-      return
-    }
-    //Update the story with the new word
-    let newStoryData = storyData + ' ' + newWord
-    //Write the entire story back to the file
-    try {
-      const filepath = path.join(__dirname, 'story.txt')
-      //write back to the file
-      fs.writeFile(filepath, newStoryData, "utf8", (err) =>{
-        if(err){
-          console.error('Story could not be changed')
-          return
-        }
-      })
-    } catch {
-      console.error('Something went wrong with the Story')
-    } 
-  })  
-  res.redirect(`${req.baseUrl}`)
-})
-
+  // Create an object of the updated story data from the request body
+  const newWord = req.body;
+  // Check if word is in english dictionary.
+  if (words.check(newWord) === true) {
+    // Read in the JSON file and locate the puppy we are going to update
+    getStoryData((err, storyData) => {
+      if (err) {
+        res.status(500).send(err.message);
+        return;
+      }
+      // Update the story with the new word
+      const newStoryData = `${storyData} ${newWord}`;
+      // Write the entire story back to the file
+      try {
+        const filepath = path.join(__dirname, 'story.txt');
+        // write back to the file
+        fs.writeFile(filepath, newStoryData, 'utf8', (err) => {
+          if (err) {
+            console.error('Story could not be changed');
+          }
+        });
+      } catch {
+        console.error('Something went wrong with the Story');
+      }
+    });
+    res.redirect(`${req.baseUrl}`);
+  }
+});
 
 // router.get('/', (req, res) => {
 //   res.render('home');
